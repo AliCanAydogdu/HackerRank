2ccd5bd10a999b8b873ce7ea6cad5715a6342580
Ali Can Aydogdu <alicantx@gmail.com>
2017-02-24 12:27:15 -0600

    Initial Commit

---
+++ Algorithm/C-sharp_Solutions/Time_Conversion.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        Console.WriteLine(DateTime.Parse(Console.ReadLine()).ToString("HH:mm:ss"));
+    }
+}
