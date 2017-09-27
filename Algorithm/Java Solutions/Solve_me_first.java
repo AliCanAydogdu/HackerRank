867552b0b6c4472ab18b007c87fb36a08aa29629
Ali Can Aydogdu <alicantx@gmail.com>
2017-09-27 17:40:02 -0500

    Update Solve_me_first.java

--- Algorithm/Java Solutions/Solve_me_first.java
+++ Algorithm/Java Solutions/Solve_me_first.java
@@ -5,15 +5,11 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-
-
     static int solveMeFirst(int a, int b) {
       return a+b;
-
    }
-
-
- public static void main(String[] args) {
+    
+public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a;
         a = in.nextInt();
